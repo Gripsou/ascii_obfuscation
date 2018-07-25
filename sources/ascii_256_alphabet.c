b0328e9b7039e7c8e9c9f887df343cced7eed75c
Vincent <vincent@project-tomorrow.fr>
2018-07-25 19:35:42 +0200

    Add two example C files
    The two file contain excatly the same code but written in two different styles to show, how complex it can be to read even the simpliest program if style does not fit (we use K&R function declaration for worse understanding)

---
+++ sources/ascii_256_alphabet.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+main(){  /* Woohoooo!!! K&R style!!! */
+    for(int i = 0; i < 256 ; i++) printf("idx : %d --> %c\n", i, i);  /* Loop on one line! Yeah! */
+}
---
+++ sources/ascii_256_alphabet_tidy.c
@@ -0,0 +1,17 @@
+/**
+  * @brief ```ascii_256_alphabet.c``` tidied up version
+  * @remark This looks much nicer than the K&R style isn't it
+  * @details Some terminals are printing out character foor values that are greater than ```0x7F```
+  *          we would like to see which
+  */
+
+#include <stdio.h>
+
+void main( void )
+{
+    int i = 0;
+    for( i = 0 ; i < 256 ; i++ )
+    {
+        printf("idx : %d --> %c\n", i, i);
+    }
+}
